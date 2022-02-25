bbee3d0f8f499d2c31162d053078ffd96fb6ec27
Anna Couto <annacouto@usp.br>
2022-02-25 14:24:52 -0300

    Update to github

---
+++ R/normalize.r
@@ -0,0 +1,138 @@
+#' Normalize
+#'
+#' This function normalize data.
+#' @export
+#' @param myDir Path to the directory of work.
+#' @param pslist List of spectra.
+#' @param metadata A matrix or data.frame with metadata information about samples. Include, at least 'sample' and 'file' columns with name of sample and its path, respectively. More information can be added in new columns, such as 'group', 'class', 'biorep' and 'tecrep'.
+#' @param anIC A 'xsAnnotate' CAMER object with grouped spectra.
+#' @importFrom grDevices dev.off pdf png tiff
+#' @importFrom graphics boxplot grid legend par text
+#' @importFrom methods as new
+#' @importFrom stats cor sd t.test var
+#' @importFrom utils choose.dir menu read.csv select.list write.csv write.table
+#' @importFrom tools file_path_as_absolute
+#' @import NormalyzerDE
+#' @import ggplot2
+#' @examples
+#' normlized <- normalize(anIC, pslist, metadata, myDir)
+normalize <- function(anIC, pslist, metadata, myDir) {
+  # check if representative ions are ok
+  okay <- 2
+  while (okay == 2) {
+    pre_anno <- read.csv("pre_anno.csv", sep = ";", dec = ",", na.string = c("NA, "))
+    quant <- matrix(nrow = nrow(pre_anno), ncol = 5 + (2 * nrow(metadata)))
+    colnames(quant) <- c("id, Fragment Ion (m/z Quant), Compound Name, Chemical Formula, Metabolic Class", metadata$sample, metadata$sample)
+    quant[, "id"] <- pre_anno[, "id"]
+    quant[, "Compound Name"] <- pre_anno[, "annotation"]
+
+    # set to folder
+    setwd("Statistics")
+
+    # ask about derivatizations
+    der <- menu(c("Yes, No"), graphics = TRUE, title = "Are those samples derivatized with trimethylsilyl?")
+    for (i in 1:nrow(quant)) {
+      temp <- anIC@pspectra[[as.integer(quant[i, 1])]]
+      if (der == 1 && 73 %in% pslist[[i]]@spectrum[, 1]) { # if der=1, ion m/z 73 is present
+        x <- sort(pslist[[i]]@spectrum[-which(pslist[[i]]@spectrum[, 1] == 73), 2], decreasing = TRUE) # remove m/z 73 from possibilities of representative ion
+      } else {
+        x <- sort(pslist[[i]]@spectrum[, 2], decreasing = TRUE)
+      }
+      y <- rbind(anIC@groupInfo[temp, ])
+      z <- rbind(y[which(y == x[1], arr.ind = TRUE)[, 1], ], y[which(y == x[2], arr.ind = TRUE)[, 1], ])
+      quant[i, 2] <- paste0(z[1, 1], , , z[2, 1])
+      quant[i, 6:(5 + nrow(metadata))] <- z[1, which(colnames(z) == "X1"):which(colnames(z) == paste0("X", nrow(metadata)))]
+      quant[i, (6 + nrow(metadata)):(5 + 2 * nrow(metadata))] <- z[2, which(colnames(z) == "X1"):which(colnames(z) == paste0("X", nrow(metadata)))]
+    }
+    mat <- quant[, 6:ncol(quant)]
+    mat <- apply(mat, c(1, 2), FUN = as.numeric)
+
+    # write non normalized data into .csv file
+    write.csv(quant, "NotNormalized_quantification.csv", row.names = FALSE, na = "")
+
+    # Normalyzer: evaluation and picking of normalization method for the spectra intensities (only most intense peak)
+    x <- as.data.frame(mat)
+    x[x == 0] <- NA
+    x[x > 0 & x < 1] <- 0
+    write.table(x[, 1:nrow(metadata)], "data.tsv", sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
+    write.table(metadata, "design.tsv", sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
+
+    # set up for NormalyzerDE
+    designFp <- file_path_as_absolute("design.tsv")
+    dataFp <- file_path_as_absolute("data.tsv")
+    normalyzer(jobName = "Normalyzer_results", designPath = designFp, dataPath = dataFp, outputDir = myDir, sampleColName = "sample", groupColName = "tec_rep", requireReplicates = FALSE)
+
+    # Pick method and apply
+    fill <- list.files(paste0(myDir, "/, Normalyzer_results"), full.names = TRUE, pattern = "-normalized.txt", recursive = TRUE)
+    norms <- sub("-normalized.txt", replacement = "", fixed = TRUE, x = basename(fill))
+    bestNormMat <- menu(norms, graphics = TRUE, title = "Choose the best normalization")
+    if (norms[bestNormMat] == "CycLoess") {
+      mat <- performCyclicLoessNormalization(mat)
+    }
+    if (norms[bestNormMat] == "GI") {
+      mat <- globalIntensityNormalization(mat)
+    }
+    if (norms[bestNormMat] == "log2") {
+      mat <- log2(mat)
+    }
+    if (norms[bestNormMat] == "mean") {
+      mat <- meanNormalization(mat)
+    }
+    if (norms[bestNormMat] == "median") {
+      mat <- medianNormalization(mat)
+    }
+    if (norms[bestNormMat] == "Quantile") {
+      mat <- performQuantileNormalization(mat)
+    }
+    if (norms[bestNormMat] == "RLR") {
+      mat <- performGlobalRLRNormalization(mat)
+    }
+    if (norms[bestNormMat] == "VSN") {
+      mat <- performVSNNormalization(mat)
+    }
+    n <- as.data.frame(cbind(quant[, 1:5], mat))
+
+    # Confirm if the chosen ions are indeed from the same spectrum
+    # if everything is ok, use only first ion for the rest of statistics
+    y <- data.frame()
+    for (ii in 6:(5 + nrow(metadata))) {
+      for (i in 1:nrow(n)) {
+        y[i, (ii - 5)] <- as.double(n[i, ii]) / as.double(n[i, (ii + nrow(metadata))])
+      }
+    }
+    y[, nrow(metadata) + 1] <- apply(y[, 1:nrow(metadata)], MARGIN = 1, FUN = var, na.rm = TRUE)
+    y[, nrow(metadata) + 2] <- apply(y[, 1:nrow(metadata)], MARGIN = 1, FUN = sd, na.rm = TRUE)
+    rownames(y) <- n$id
+    colnames(y) <- c(metadata$sample, "Variance, sd")
+    a <- ggplot(y, aes(x = y[, nrow(metadata) + 1])) +
+      geom_histogram() +
+      ggtitle("Variance Distribution") +
+      xlab("Variance") +
+      ylab("Frequency") +
+      theme(rect = element_rect(fill = "transparent")) # histograma da variância. O ideal é que tenha pouca variância alta
+    b <- ggplot(y, aes(x = y[, nrow(metadata) + 2])) +
+      geom_histogram() +
+      ggtitle("Standard Deviation") +
+      xlab("Standard Deviation") +
+      ylab("Frequency") +
+      theme(rect = element_rect(fill = "transparent")) # histograma do desvio padrão.
+
+    # plot histogram of variance and standard deviation
+    png("variance_dp.png", units = "cm", width = 16, height = 16, res = 1500, bg = "NA")
+    gridExtra::grid.arrange(a, b, ncol = 1)
+    dev.off()
+    tiff("variance_dp.tiff", units = "cm", width = 16, height = 16, res = 1500, bg = "NA")
+    gridExtra::grid.arrange(a, b, ncol = 1)
+    dev.off()
+    okay <- menu(c("OK, keep going, No, re-do normalization"), graphics = TRUE, title = "Are the results ok?")
+  }
+
+  n <- n[, 1:(5 + nrow(metadata))] # as everything is ok, use only first most intense ions for representative
+  write.csv(n, "Normalized_quantification.csv", row.names = FALSE, na = "")
+
+  # set to main folder
+  setwd(myDir)
+
+  # return results
+  return(n)
+}
