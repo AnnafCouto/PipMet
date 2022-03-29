fa31493dd1aa63b75cd4c68ee8ad67045b714341
Anna <annacouto@usp.br>
2022-03-29 10:23:32 -0300

    Removed 'example' argument of process

--- R/GC_dataProcess.r
+++ R/GC_dataProcess.r
@@ -73,7 +73,7 @@ GC_dataProcess <- function(myDir = NULL, sample_dir = NULL, metadata = NULL, ext
   rm(read)
 
   # process samples
-  quiet(xdata4 <- process(raw_data, metadata, myDir, colors, EIC, ions, pictures, example))
+  quiet(xdata4 <- process(raw_data, metadata, myDir, colors, EIC, ions, pictures))
 
   # define spectra and create .msp files
   quiet(spectra <- getSpectra(xdata4))
