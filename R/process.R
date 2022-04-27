857472d89061d200a6a6d8843f33fc33205cab70
Anna <annacouto@usp.br>
2022-04-27 09:18:10 -0300

    Updated code for 1 file processing possibility

--- R/process.R
+++ R/process.R
@@ -42,130 +42,135 @@ process <- function(raw_data, metadata, myDir, colors, EIC = 2, ions = NULL, pic
     }
   }
 
-  # retention time correction
-  xdata2 <- adjustRtime(xdata, param = ObiwarpParam(binSize = 0.6))
-  if(!example == TRUE) {save(xdata2, file = "xdata2.RData")}
+  # check if there is more than one file
+  if (nrow(xdata@phenoData@data) > 1) {
+    # retention time correction
+    xdata2 <- adjustRtime(xdata, param = ObiwarpParam(binSize = 0.6))
+    if(!example == TRUE) {save(xdata2, file = "xdata2.RData")}
 
-  if (example == FALSE) {
-    # ask condition to compare from the metadata table
-    x <- menu(colnames(metadata), graphics = TRUE, title = "Choose conditions (from metadata table) to group samples: ")
-  } else {
-    x <- 2
-  }
-
-  # grouping peaks
-  xdata3 <- groupChromPeaks(xdata2, param = PeakDensityParam(sampleGroups = metadata[, x], bw = 0.5, minSamples = 1, maxFeatures = 500, minFraction = 0.4))
-  if(!example == TRUE) {save(xdata3, file = "xdata3.RData")}
-
-  # imagens dos dados em processamento e pós processamento (padrões, cromatogramas de íon extraído)
-  if (exists("xdata3") & pictures == TRUE) {
-    # create and set folder for images
-    if (!dir.exists("peakProcessing_results")) {dir.create("peakProcessing_results")}
-    setwd("peakProcessing_results")
-
-    # heatmap of identified peaks per region of chromatogram
-    # tiff
-    tiff("plotChromPeakImage.tiff", units = "cm", width = 16, height = 16, res = 900, bg = "NA")
-    par(mar = c(5, 9, 4, 1) + .1)
-    plotChromPeakImage(xdata3)
-    dev.off()
-    # png
-    png("plotChromPeakImage.png", units = "cm", width = 16, height = 16, res = 900, bg = "NA")
-    par(mar = c(5, 9, 4, 1) + .1)
-    plotChromPeakImage(xdata3)
-    dev.off()
-
-    # boxplot of log2 intensities per sample
-    ints <- split(log2(chromPeaks(xdata3)[, "into"]),
-      f = chromPeaks(xdata3)[, "sample"]
-    )
-    names(ints) <- metadata$all2
-    for (i in 1:length(colors)) {
-      # tiff
-      tiff(paste0(names(colors)[i], "_boxplotLog2Postprocessed.tiff"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
-      par(mar = c(7, 5, 3, 1) + .1, cex.axis = 1)
-      boxplot(ints,
-        varwidth = TRUE, col = colors[[i]][[2]][colors[[i]][[1]]],
-        ylab = expression(log[2] ~ intensity), main = "Peak intensities", las = 3, xaxt = "n"
-      )
-      grid(nx = NA, ny = NULL)
-      text(seq_along(metadata$all2), par("usr")[3],
-        labels = metadata$all2, srt = 45, adj = c(1.1, 1.1), xpd = TRUE, cex = 0.7
-      )
-      dev.off()
-      # png
-      png(paste0(names(colors)[i], "_boxplotLog2Postprocessed.png"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
-      par(mar = c(7, 5, 3, 1) + .1, cex.axis = 1)
-      boxplot(ints,
-        varwidth = TRUE, col = colors[[i]][[2]][colors[[i]][[1]]],
-        ylab = expression(log[2] ~ intensity), main = "Peak intensities", las = 3, xaxt = "n"
-      )
-      grid(nx = NA, ny = NULL)
-      text(seq_along(metadata$all2), par("usr")[3],
-        labels = metadata$all2, srt = 45, adj = c(1.1, 1.1), xpd = TRUE, cex = 0.7
-      )
-      dev.off()
+    if (example == FALSE) {
+      # ask condition to compare from the metadata table
+      x <- menu(colnames(metadata), graphics = TRUE, title = "Choose conditions (from metadata table) to group samples: ")
+    } else {
+      x <- 2
     }
 
-    # chromatogram postprocessed
-    bpc_after <- chromatogram(xdata3, aggregationFun = "max", include = "none")
-    for (i in 1:length(colors)) {
+    # grouping peaks
+    xdata3 <- groupChromPeaks(xdata2, param = PeakDensityParam(sampleGroups = metadata[, x], bw = 0.5, minSamples = 1, maxFeatures = 500, minFraction = 0.4))
+    if(!example == TRUE) {save(xdata3, file = "xdata3.RData")}
+
+    # imagens dos dados em processamento e pós processamento (padrões, cromatogramas de íon extraído)
+    if (exists("xdata3") & pictures == TRUE) {
+      # create and set folder for images
+      if (!dir.exists("peakProcessing_results")) {dir.create("peakProcessing_results")}
+      setwd("peakProcessing_results")
+
+      # heatmap of identified peaks per region of chromatogram
       # tiff
-      tiff(paste0(names(colors)[i], "_postprocessedChromatogram.tiff"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
-      par(mfrow = c(2, 1), mar = c(4.5, 4.2, 1, 0.5))
-      plot(bpc_after, col = colors[[i]][[2]][colors[[i]][[1]]])
-      legend("topright", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 1, bg = "transparent")
-      plotAdjustedRtime(xdata3, col = colors[[i]][[2]][colors[[i]][[1]]])
-      legend("bottomright", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 1, bg = "transparent")
+      tiff("plotChromPeakImage.tiff", units = "cm", width = 16, height = 16, res = 900, bg = "NA")
+      par(mar = c(5, 9, 4, 1) + .1)
+      plotChromPeakImage(xdata3)
       dev.off()
       # png
-      png(paste0(names(colors)[i], "_postprocessedChromatogram.png"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
-      par(mfrow = c(2, 1), mar = c(4.5, 4.2, 1, 0.5))
-      plot(bpc_after, col = colors[[i]][[2]][colors[[i]][[1]]])
-      legend("topright", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 1, bg = "transparent")
-      plotAdjustedRtime(xdata3, col = colors[[i]][[2]][colors[[i]][[1]]])
-      legend("bottomright", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 1, bg = "transparent")
+      png("plotChromPeakImage.png", units = "cm", width = 16, height = 16, res = 900, bg = "NA")
+      par(mar = c(5, 9, 4, 1) + .1)
+      plotChromPeakImage(xdata3)
       dev.off()
-    }
 
-    # cromatograma de íons extraído
-    if (EIC == 1) {
-      dir.create("Monitoring ions")
-      setwd("Monitoring ions")
+      # boxplot of log2 intensities per sample
+      ints <- split(log2(chromPeaks(xdata3)[, "into"]),
+        f = chromPeaks(xdata3)[, "sample"]
+      )
+      names(ints) <- metadata$all2
+      for (i in 1:length(colors)) {
+        # tiff
+        tiff(paste0(names(colors)[i], "_boxplotLog2Postprocessed.tiff"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
+        par(mar = c(7, 5, 3, 1) + .1, cex.axis = 1)
+        boxplot(ints,
+          varwidth = TRUE, col = colors[[i]][[2]][colors[[i]][[1]]],
+          ylab = expression(log[2] ~ intensity), main = "Peak intensities", las = 3, xaxt = "n"
+        )
+        grid(nx = NA, ny = NULL)
+        text(seq_along(metadata$all2), par("usr")[3],
+          labels = metadata$all2, srt = 45, adj = c(1.1, 1.1), xpd = TRUE, cex = 0.7
+        )
+        dev.off()
+        # png
+        png(paste0(names(colors)[i], "_boxplotLog2Postprocessed.png"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
+        par(mar = c(7, 5, 3, 1) + .1, cex.axis = 1)
+        boxplot(ints,
+          varwidth = TRUE, col = colors[[i]][[2]][colors[[i]][[1]]],
+          ylab = expression(log[2] ~ intensity), main = "Peak intensities", las = 3, xaxt = "n"
+        )
+        grid(nx = NA, ny = NULL)
+        text(seq_along(metadata$all2), par("usr")[3],
+          labels = metadata$all2, srt = 45, adj = c(1.1, 1.1), xpd = TRUE, cex = 0.7
+        )
+        dev.off()
+      }
 
-      for (ii in 1:length(ions)) {
-        crom <- chromatogram(xdata3, rt = c(ions[[ii]][["rt"]] - 5, ions[[ii]][["rt"]] + 5), mz = ions[[ii]][["mz"]], include = "none")
-        for (i in 1:length(colors)) {
-          # tiff
-          tiff(paste0(names(colors)[i], "_", ii, "_postPross_EIC.tiff"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
-          plot(crom, col = colors[[i]][[2]][colors[[i]][[1]]])
-          legend("right", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 0.8, bg = "transparent")
-          dev.off()
-          # png
-          png(paste0(names(colors)[i], "_", ii, "_postPross_EIC.png"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
-          plot(crom, col = colors[[i]][[2]][colors[[i]][[1]]])
-          legend("right", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 0.8, bg = "transparent")
-          dev.off()
+      # chromatogram postprocessed
+      bpc_after <- chromatogram(xdata3, aggregationFun = "max", include = "none")
+      for (i in 1:length(colors)) {
+        # tiff
+        tiff(paste0(names(colors)[i], "_postprocessedChromatogram.tiff"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
+        par(mfrow = c(2, 1), mar = c(4.5, 4.2, 1, 0.5))
+        plot(bpc_after, col = colors[[i]][[2]][colors[[i]][[1]]])
+        legend("topright", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 1, bg = "transparent")
+        plotAdjustedRtime(xdata3, col = colors[[i]][[2]][colors[[i]][[1]]])
+        legend("bottomright", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 1, bg = "transparent")
+        dev.off()
+        # png
+        png(paste0(names(colors)[i], "_postprocessedChromatogram.png"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
+        par(mfrow = c(2, 1), mar = c(4.5, 4.2, 1, 0.5))
+        plot(bpc_after, col = colors[[i]][[2]][colors[[i]][[1]]])
+        legend("topright", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 1, bg = "transparent")
+        plotAdjustedRtime(xdata3, col = colors[[i]][[2]][colors[[i]][[1]]])
+        legend("bottomright", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 1, bg = "transparent")
+        dev.off()
+      }
+
+      # cromatograma de íons extraído
+      if (EIC == 1) {
+        dir.create("Monitoring ions")
+        setwd("Monitoring ions")
+
+        for (ii in 1:length(ions)) {
+          crom <- chromatogram(xdata3, rt = c(ions[[ii]][["rt"]] - 5, ions[[ii]][["rt"]] + 5), mz = ions[[ii]][["mz"]], include = "none")
+          for (i in 1:length(colors)) {
+            # tiff
+            tiff(paste0(names(colors)[i], "_", ii, "_postPross_EIC.tiff"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
+            plot(crom, col = colors[[i]][[2]][colors[[i]][[1]]])
+            legend("right", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 0.8, bg = "transparent")
+            dev.off()
+            # png
+            png(paste0(names(colors)[i], "_", ii, "_postPross_EIC.png"), units = "cm", width = 16, height = 16, res = 900, bg = "NA")
+            plot(crom, col = colors[[i]][[2]][colors[[i]][[1]]])
+            legend("right", legend = names(colors[[i]][[2]]), col = colors[[i]][[2]], fill = colors[[i]][[2]], box.lty = 0, cex = 0.8, bg = "transparent")
+            dev.off()
+          }
         }
       }
-    }
 
-    # set to main folder
-    setwd(myDir)
+      # set to main folder
+      setwd(myDir)
+    }
   }
 
-  xdata4 <- as(xdata3, "xcmsSet")
-  if ("group" %in% colnames(metadata)) {
-    sampclass(xdata4) <- metadata$group
-  } else {
-    if ("class" %in% colnames(metadata)) {
-      sampclass(xdata4) <- metadata$class
+  if (exists ('xdata3')) {
+    xdata4 <- as(xdata3, "xcmsSet")
+    if ("group" %in% colnames(metadata)) {
+      sampclass(xdata4) <- metadata$group
     } else {
-      sampclass(xdata4) <- NA
+      if ("class" %in% colnames(metadata)) {
+        sampclass(xdata4) <- metadata$class
+      } else {
+        sampclass(xdata4) <- NA
+      }
     }
-  }
-  xdata4 <- fillPeaks(xdata4)
-  if(!example == TRUE) {save(xdata4, file = 'xdata4.RData')}
+    xdata4 <- fillPeaks(xdata4)
+    if(!example == TRUE) {save(xdata4, file = 'xdata4.RData')}
+  } else {xdata4 <- as(xdata, "xcmsSet")}
 
   # return results
   return(xdata4)
